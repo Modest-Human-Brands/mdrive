f10891f17a3c5b1b15fadf326f2ca7cfeb0c199c
Shirsendu Bairagi <shirsendu2001@gmail.com>
2026-03-28 12:27:27 +0530

    fix: eslint issue

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import unjs from 'eslint-config-unjs'
 
 export default unjs({
-  ignores: ['node_modules', 'dist', 'static', 'temp'],
+  ignores: ['node_modules', '.output', 'static', 'temp'],
   rules: {
     'unicorn/no-anonymous-default-export': 0,
   },
